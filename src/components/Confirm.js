069cd3dc7b3cde312a2c138b4d43f6138b18dbb8
James II <jcabell2288@gmail.comm>
2020-01-08 21:16:09 -0600

    first round completion

---
+++ src/components/Confirm.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import {List, ListItem} from 'material-ui/List'
+import RaisedButton from 'material-ui/RaisedButton'
+
+export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        //PROCESS FROM //
+        this.props.nextStep();
+    }
+
+    back = e => {
+        e.preventDefault();
+        this.props.prevStep();
+    }
+
+    render() {
+        const {values: {firstName, lastName, email, occupation, city, bio}} = this.props;
+
+        return (
+            <MuiThemeProvider>
+               <React.Fragment>
+                   <AppBar title="Confirm User Data"></AppBar>
+                   <List>
+                       <ListItem
+                       primaryText="First Name"
+                       secondaryText={firstName}
+                       >    
+                       </ListItem>
+                       <ListItem
+                       primaryText="Last Name"
+                       secondaryText={lastName}
+                       >    
+                       </ListItem>
+                       <ListItem
+                       primaryText="Email"
+                       secondaryText={email}
+                       >    
+                       </ListItem>
+                       <ListItem
+                       primaryText="Occupation"
+                       secondaryText={occupation}
+                       >    
+                       </ListItem>
+                       <ListItem
+                       primaryText="city"
+                       secondaryText={bio}
+                       >    
+                       </ListItem>
+                   </List>
+                   <br/>
+                   <RaisedButton
+                        label="Confirm & Continue"
+                        primary={true} 
+                        style={styles.button}
+                        onClick={this.continue}
+                    ></RaisedButton>
+                    <RaisedButton
+                        label="Back"
+                        primary={false} 
+                        style={styles.button}
+                        onClick={this.back}
+                    ></RaisedButton>
+               </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+const styles = {
+    button: {
+        margin: 15
+    }
+}
+
+export default FormUserDetails
